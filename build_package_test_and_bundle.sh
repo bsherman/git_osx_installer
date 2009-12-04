197057db6b850c77ff6ee4606355e1938815bed8
Benjamin Sherman <benjamin@anduril.blade.holyarmy.org>
2009-12-03 18:39:47 -0800

    Use variables more consistently and be a bit more robust.

--- build_package_test_and_bundle.sh
+++ build_package_test_and_bundle.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+DISK_IMAGE="Disk Image"
 # remove old installers
-rm Disk\ Image/*.pkg
+rm *.dmg
+if [ -d "$DISK_IMAGE" ]; then
+     rm "$DISK_IMAGE/*.pkg"
+else 
+     mkdir "$DISK_IMAGE"
+fi
 
 ./build.sh
 
 GIT_VERSION=$(git --version | sed 's/git version //')
 PACKAGE_NAME="git-$GIT_VERSION-intel-leopard"
-IMAGE_FILENAME="git-$GIT_VERSION-intel-leopard.dmg" 
+IMAGE_FILENAME="$PACKAGE_NAME.dmg" 
+
+#cleanup post-build
+sudo rm -fr git_build/git-$GIT_VERSION
 
 echo $PACKAGE_NAME | pbcopy
 echo "Git version is $GIT_VERSION"
 
-/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc Git\ Installer.pmdoc/ -o Disk\ Image/git-$GIT_VERSION-intel-leopard.pkg --title "Git $GIT_VERSION"
+/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --doc Git\ Installer.pmdoc/ -o "$DISK_IMAGE/$PACKAGE_NAME.pkg" --title "Git $GIT_VERSION"
 
 echo "Testing the installer..."
 
@@ -20,8 +29,8 @@ echo "Testing the installer..."
 
 printf "$GIT_VERSION" | pbcopy
 
-UNCOMPRESSED_IMAGE_FILENAME="git-$GIT_VERSION-intel-leopard.uncompressed.dmg"
-hdiutil create $UNCOMPRESSED_IMAGE_FILENAME -srcfolder "Disk Image" -volname "Git $GIT_VERSION Intel Leopard" -ov
+UNCOMPRESSED_IMAGE_FILENAME="$PACKAGE_NAME.uncompressed.dmg"
+hdiutil create $UNCOMPRESSED_IMAGE_FILENAME -srcfolder "$DISK_IMAGE" -volname "Git $GIT_VERSION Intel Leopard" -ov
 hdiutil convert -format UDZO -o $IMAGE_FILENAME $UNCOMPRESSED_IMAGE_FILENAME
 rm $UNCOMPRESSED_IMAGE_FILENAME
 
